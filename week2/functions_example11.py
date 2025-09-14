5652dd5adfb718495c206acef6eb7b7e9901a8ba
Christos Gogos <chgogos@gmail.com>
2025-09-15 00:04:58 +0300

    week2

--- week2/functions_example11.py
+++
@@ -1,6 +0,0 @@
-def greet():
-    message = "Hello, world!"  # τοπική μεταβλητή
-    print(message)             # χρησιμοποιείται μέσα στη συνάρτηση
-
-greet()           # ✅ 
-print(message)    # ❌ Error: NameError: name 'message' is not defined
